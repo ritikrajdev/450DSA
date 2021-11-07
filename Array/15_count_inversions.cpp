33da85ec50ec6d7ba56888b5fbcfe1dfc15f7580
ritikrajdev <ritikrajdev761@gmail.com>
2021-11-07 07:18:34 +0530

    Array - 15

--- Array/15_count_inversions.cpp
+++ Array/15_count_inversions.cpp
@@ -7,16 +7,47 @@
 using namespace std;
 
 namespace ritik {
-	int count_inversions(vector<int>& vec) {
-		// Naive n^2
-		int count = 0;
-		for (int i = 0; i < vec.size(); i++) {
-			for (int j = i+1; j < vec.size(); j++) {
-				if (vec.at(j) < vec.at(i))
-					count++;
-			}
+	int merge(vector<int>& vec, int l, int m, int r) {
+		vector<int> res(r-l+1);
+		int k = 0, cnt = 0;
+
+		int i = l, j = m;
+		
+		while (i < m && j <= r) {
+			if (vec.at(i) > vec.at(j))
+				cnt += m - i, res.at(k++) = vec.at(j++);
+			else
+				res.at(k++) = vec.at(i++);
 		}
-		return count;
+
+		while (i < m)
+			res.at(k++) = vec.at(i++);
+
+		while (j <= r)
+			res.at(k++) = vec.at(j++);
+
+		for (int x = l; x <= r; x++)
+			vec.at(x) = res.at(x - l);
+
+		return cnt;
+	}
+
+	int merge_sort(vector<int>& vec, int l, int r) {
+		int cnt = 0;
+		if (r > l) {
+			int m = (r + l) / 2;
+			
+			cnt += merge_sort(vec, l, m);
+			cnt += merge_sort(vec, m+1, r);
+
+			cnt += merge(vec, l, m+1, r);
+		}
+		return cnt;
+	}
+
+	int count_inversions(vector<int>& vec) {
+		int count = 0;
+		return merge_sort(vec, 0, vec.size() - 1);
 	}
 }
 
