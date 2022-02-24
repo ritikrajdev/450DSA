e70f2aa5a2a2a73f54f42d94f4413f015d6b0008
ritikrajdev <ritikrajdev761@gmail.com>
2022-02-24 05:51:53 +0530

    String - 16 -- Optimisation

--- String/16_word_break.cpp
+++ String/16_word_break.cpp
@@ -1,25 +1,28 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
-  public:
+    unordered_map<string, bool> ump;
+public:
     bool wordBreak(string A, vector<string> &B) {
         if (A == "")
             return true;
+        
+        if (ump.find(A) != ump.end())
+            return ump[A];
 
-        for (auto x : B) {
+        for (auto x: B) {
             auto i = A.find(x);
             if (i != string::npos) {
                 bool l = wordBreak(A.substr(0, i), B);
-                bool r = i + x.length() < A.length()
-                             ? wordBreak(A.substr(i + x.length()), B)
-                             : true;
+                bool r = i + x.length() < A.length() ? wordBreak(A.substr(i+x.length()), B) : true;
                 if (l && r)
-                    return true;
+                    return ump[A] = true;
             }
         }
-        return false;
+        return ump[A] = false;
     }
 };
