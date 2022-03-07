34073a1716c27b068a0c192d673b7af4e424d26e
ritikrajdev <ritikrajdev761@gmail.com>
2022-03-07 17:44:41 +0530

    Binary Tree - 10 -- Update

--- Binary Trees/10_top_view.cpp
+++ Binary Trees/10_top_view.cpp
@@ -1,42 +1,23 @@
 class Solution
 {
-    vector<int> left;
-    vector<int> right;
     public:
-    
-    void leftTopView(Node* root, int vertical_level) {
-        if (!root)
-            return;
-        
-        if (vertical_level == -left.size())
-            left.push_back(root->data);
-        
-        leftTopView(root->left, vertical_level-1);
-        leftTopView(root->right, vertical_level+1);
-    }
-    
-    void rightTopView(Node* root, int vertical_level) {
-        if (!root)
-            return;
-        
-        if (vertical_level == right.size())
-            right.push_back(root->data);
-        
-        rightTopView(root->right, vertical_level+1);
-        rightTopView(root->left, vertical_level-1);
-    }
-
-
     vector<int> topView(Node *root) {
-        leftTopView(r, 0);
-        rightTopView(r, 0);
-
-        reverse(left.begin(), left.end());
-
-        // 0th will be root
-        for (int i = 1; i < right.size(); i++)
-            left.push_back(right[i]);
-
-        return left;
+        map<int, Node*> mp;
+        queue<pair<Node*, int>> q;
+        
+        q.push({root, 0});
+        while (!q.empty()) {
+            if (!mp[q.front().second])
+                mp[q.front().second] = q.front().first;
+            if (q.front().first->left) q.push({q.front().first->left, q.front().second-1});
+            if (q.front().first->right) q.push({q.front().first->right, q.front().second+1});
+            q.pop();
+        }
+        
+        vector<int> ans;
+        for (auto x: mp)
+            ans.push_back(x.second->data);
+        
+        return ans;
     }
 };
